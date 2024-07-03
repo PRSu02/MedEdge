08d1af6ada89eecda02365487b210d5d15fc6b94
Priyangshu Deka <137042867+PRSu02@users.noreply.github.com>
2024-07-03 10:40:45 +0530

    Update node_manage.py

--- infrastucture/node_manage.py
+++ infrastucture/node_manage.py
@@ -1,17 +1,19 @@
-## Node Management
-
 import os
 
-NODES_FILE = 'nodes.txt'
+NODES_FILE = './nodes.txt'
 
 def read_nodes():
+    if not os.path.exists(NODES_FILE):
+        return []
     with open(NODES_FILE, 'r') as file:
         nodes = [line.strip() for line in file.readlines()]
     return nodes
 
 def add_node(ip):
-    with open(NODES_FILE, 'a') as file:
-        file.write(ip + '\n')
+    nodes = read_nodes()
+    if ip not in nodes:
+        with open(NODES_FILE, 'a') as file:
+            file.write(ip + '\n')
 
 def remove_node(ip):
     nodes = read_nodes()
